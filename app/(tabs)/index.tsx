52705ad214a84ccc9e1461803e1b762f3b4bad39
AmNat789 <amit.amanna@code.berlin>
2025-09-02 12:50:35 +0200

    feature:add tailwind support

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title" className="bg-red-500">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
