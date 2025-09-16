03f3f46fa653d1b1905539b10e615d26ae56d7f5
AmNat789 <amit.natheniel@gmail.com>
2025-09-16 11:25:51 +0200

    feature:Performance Section

--- app/(tabs)/wellbeing.tsx
+++ app/(tabs)/wellbeing.tsx
@@ -1,5 +1,6 @@
 import { ArrowRight } from "@/components/icons";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
+import PerformanceSection from "@/components/wellbeing/PerformanceSection";
 import SleepSection from "@/components/wellbeing/SleepSection";
 import StrainSection from "@/components/wellbeing/StrainSection";
 import StressSection from "@/components/wellbeing/StressSection";
@@ -21,6 +22,7 @@ export default function WellbeingPage() {
                 <Text className="effra-medium text-2xl">{t('title')}</Text>
                 {isRTL ? <ClickableArrow isRTL={isRTL}/> : <View/>}
             </View>
+            <PerformanceSection performance={85} performance14DaysHistory={[23, 45, 100, 12, 89, 34, 56, 78, 91, 15, 42, 73, 28, 85]} />
             <StrainSection strainToday={12} strain14Days={15.5} />
             <StressSection stress={7} stress14Days={4} />
             <SleepSection rem={12} sws={15.5} light={18} awake={21} score={0.8} />
