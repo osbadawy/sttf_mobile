35243e28db32529164356fc0de8be7960fb86a46
Omar Badawy <omar.badawy@hotmail.com>
2025-10-20 17:52:12 +0200

    feat: nutrition graph page set

--- app/player/nutrition/NutritionDashboard.tsx
+++ app/player/nutrition/NutritionDashboard.tsx
@@ -1,3 +1,24 @@
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { useLocalization } from "@/contexts/LocalizationContext";
+import { Text, View } from "react-native";
+
 export default function NutritionDashboard() {
-    return null;
+    const { t } = useLocalization("components.nutrition.nutritionList");
+
+
+    return (
+        <ParallaxScrollView
+          headerProps={{
+            title: t("nutritionDashboard"),
+            showBackButton: true,
+            showDateSelector: false,
+            showBGImage: false,
+            showCalendarIcon: true,
+          }}
+        >
+            <View className="p-5">
+                <Text className="text-2xl font-semibold text-left">{t("header")}</Text>
+            </View>
+        </ParallaxScrollView>
+    );
 }
